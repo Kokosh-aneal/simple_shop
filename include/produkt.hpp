33d743d0d1b9d0b5bd6f39d8c8946d49f05a21c8
Jakub Kokoszka <j.kokoszka@stud.elka.pw.edu.pl>
2020-04-27 17:27:36 +0200

    dodanie klasy produkt

---
+++ include/produkt.hpp
@@ -0,0 +1,16 @@
+#ifndef __PRODUKT__H__
+#define __PRODUKT__H__
+
+#include <string.h>
+
+class Produkt{
+    private:
+        float cena;
+        string nazwa;
+        string 
+    public:
+        Produkt();
+        dodaj();
+        usun();
+        price_change();
+};

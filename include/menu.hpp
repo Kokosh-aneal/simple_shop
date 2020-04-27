d168576886f5b4b5c9ca28e69eaf96c016a343c5
Jakub Kokoszka <j.kokoszka@stud.elka.pw.edu.pl>
2020-04-27 11:51:16 +0200

    dodanie menu.hpp

---
+++ include/menu.hpp
@@ -0,0 +1,15 @@
+#ifndef __MENU__H__
+#define __MENU__H__
+
+#include <iostream>
+
+class Menu{
+    private:
+        int opcja;
+    public:
+        Menu();
+        ~Menu();
+        void show();
+}
+
+#endif  //__MENU__H__

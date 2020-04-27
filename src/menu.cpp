48fa15c45af2a9839269cf6a6a268b7e82fe7df9
Jakub Kokoszka <j.kokoszka@stud.elka.pw.edu.pl>
2020-04-27 11:59:25 +0200

    Dodanie pliku menu.cpp oraz napisanie prostego UI

---
+++ src/menu.cpp
@@ -0,0 +1,11 @@
+#include "../include/menu.hpp"
+
+void Menu::show(){
+    std::cout << "Witaj w programie zarzadzajacym sklepem!" << std::endl;
+    std::cout << "1. Dodaj produkt do magazynu" << std::endl;
+    std::cout << "2. Usuń produkt z magazynu" << std::endl;
+    std::cout << "3. Zmien cene produktu" << std::endl;
+    std::cout << "4. Zloz zamowienie na nowe produkty" << std::endl;
+    std::cout << "Wybierz opcje:";
+    std::cin >> opcja;
+}
